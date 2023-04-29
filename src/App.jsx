36414faaf6c95edd68ea39394dec51f28feb6ae4
Abdullah0095 <abdullah.web1995@gmail.com>
2023-04-30 00:31:08 +0600

    client side is started

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Home from "./Pages/Home/Home/Home";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Routes/Routes/Routes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Routes = () => {
+    return (
+        <div>
+            <h2>hhh</h2>
+        </div>
+    );
+};
+
+export default Routes;
