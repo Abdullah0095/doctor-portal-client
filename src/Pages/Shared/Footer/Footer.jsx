33e085bd9a0648b698176b3bfdada72ba64b18e1
Abdullah0095 <abdullah.web1995@gmail.com>
2023-05-14 12:01:22 +0600

    home page has done

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
-
 const Footer = () => {
-    return (
-        <div>
-            <h1>I am footer</h1>
+  return (
+    <div className="px-12 grid grid-cols-1 md:grid-cols-3 mt-5">
+      <div className="">
+        <h2 className="font-bold">SERVICES</h2>
+
+        <div className="py-4">
+          <p>Emergency Checkup</p>
+          <p>Monthly Checkup</p>
+          <p>Weekly Checkup</p>
+          <p>Deep Checkup</p>
         </div>
-    );
+      </div>
+
+      <div>
+        <h2 className="font-bold">ORAL HEALTH</h2>
+
+        <div className="py-4">
+          <p>Floride Treatement</p>
+          <p>Cavity Filling</p>
+          <p>Teath Whitening</p>
+        </div>
+      </div>
+
+      <div>
+        <h2 className="font-bold">OUR ADDRESS</h2>
+        <p className="py-4">New York - 101010 Hudson</p>
+      </div>
+
+      <footer className="font-bold  text-center">Copyright 2023 ALL Rights Reserved</footer>
+    </div>
+  );
 };
 
 export default Footer;
